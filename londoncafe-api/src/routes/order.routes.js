6878701a6a14c5efdfbcc9d03ff8c9d01eee82f0
eDigitalSolutions2024 <repositorio@edigitalsolutions.com.mx>
2026-03-23 15:16:35 -0600

    ordena aws

--- londoncafe-api/src/routes/order.routes.js
+++ londoncafe-api/src/routes/order.routes.js
@@ -3,6 +3,10 @@ const { createOrderFromApp } = require("../controllers/order.controller");
 
 const router = express.Router();
 
-router.post("/online", createOrderFromApp);
+router.get("/test", (_req, res) => {
+  res.json({ ok: true, route: "orders route alive" });
+});
+
+router.post("/from-app", createOrderFromApp);
 
 module.exports = router;
